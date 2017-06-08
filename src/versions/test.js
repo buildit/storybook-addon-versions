0ecf56246e18eec8568241532be9934c8319cb48
Panos Voudouris <panosvoudouris@gmail.com>
2017-06-08 09:50:52 +0100

    Updated versions test

--- src/versions/test.js
+++ src/versions/test.js
@@ -9,20 +9,69 @@ test('Versions dropdown not shown when no versions required (null)', () => {
   expect(tree).toMatchSnapshot();
 });
 
-test('Versions dropdown not shown when no versions required (empty)', () => {
+test('Versions dropdown not shown when no versions required (current empty)', () => {
   const tree = renderer.create(
-    <Versions versions={[]} />,
+    <Versions currentVersions="" />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Versions dropdown not shown when no versions required (current with no available)', () => {
+  const tree = renderer.create(
+    <Versions currentVersions="0.0.1" />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Versions dropdown not shown when no versions required (no current with available)', () => {
+  const tree = renderer.create(
+    <Versions availableVersions={[
+      '0.0.1',
+      '0.0.2',
+      '0.0.3',
+    ]}
+    />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Versions dropdown not shown when no versions required (empty current with available)', () => {
+  const tree = renderer.create(
+    <Versions
+      currentVersion=""
+      availableVersions={[
+        '0.0.1',
+        '0.0.2',
+        '0.0.3',
+      ]}
+    />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Versions dropdown not shown when no versions required (no current available empty)', () => {
+  const tree = renderer.create(
+    <Versions availableVersions={[]} />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Versions dropdown not shown when no versions required (available empty with current)', () => {
+  const tree = renderer.create(
+    <Versions currentVersion="0.0.1" availableVersions={[]} />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Versions dropdown shows the supplied versions', () => {
   const tree = renderer.create(
-    <Versions versions={[
-      '0.0.1',
-      '0.0.2',
-      '0.0.3',
-    ]}
+    <Versions
+      currentVersion="0.0.1"
+      availableVersions={[
+        '0.0.1',
+        '0.0.2',
+        '0.0.3',
+      ]}
     />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
