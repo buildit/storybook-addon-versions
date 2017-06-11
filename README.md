e5124bb5a9f79ad54ee5cb1dab713a4f9e57fb97
Panos Voudouris <panosvoudouris@gmail.com>
2017-06-11 17:04:54 +0100

    Sort out config

--- README.md
+++ README.md
@@ -2,7 +2,32 @@
 
 This addon attempts to get a list of available style guide versions from the root of your host. If they are found it will show a dropdown which then lets you navigate to the various versions, as such allowing users to see how a component may have changed over different versions.
 
-Currently the versions are expected to be in a file `storybook-versions.json`, listed as an array and at the root of your host. You can mock this adding a `storybook-versions.json` in your local `.storybook/` folder. Here's some sample content:
+## Storybook registration
+
+To use the plugin you just need to add a decorator. Add the following to your `config.js` in the storybook configuration:
+
+```
+import { withVersions } from '@buildit/storybook-addon-blabbr';
+
+addDecorator(withVersions);
+```
+
+## Configuration
+
+This addon expects to find a `storybook-config.json` configuration file at the root of your host, for static builds, or inside your storybook setup folder (generally `.storybook`). This should look like this:
+
+```
+{
+  "storybook": {
+    "versions": {
+      "versionsPath": "storybook-versions.json",
+      "urlRegex": ""
+    }
+  }
+}
+```
+
+`versionsPath` should point to a file in your host which will have a list of all available versions like this:
 
 ```
 [
@@ -12,3 +37,7 @@ Currently the versions are expected to be in a file `storybook-versions.json`, l
   "0.3.4"
 ]
 ```
+
+`urlRegex` will be used to determine where the version is in your URL.
+
+*TODO*: Add some sample regex here for common patterns...
--- src/storyWrapper/index.js
+++ src/storyWrapper/index.js
@@ -8,36 +8,55 @@ class StoryWrapper extends React.Component {
     this.state = {
       commentCount: 0,
       availableVersions: null,
-      currentVersion: ''
+      currentVersion: '',
     };
   }
 
   componentWillMount() {
     if (window && window.parent) {
       const url = window.parent.location;
-      const location = `${url.protocol}//${url.hostname}:${url.port}/storybook-versions.json`;
-      const currentVersion = '0.0.0';
-
-      this.setState({
-        currentVersion,
-      });
-
+      // First get the config
+      const location = `${url.protocol}//${url.hostname}:${url.port}/storybook-config.json`;
+      // TODO This is a bit horrible, I'll get round to refactoring in a bit
       fetch(location).then((response) => {
         if (response.ok) {
-          response.json().then((data) => {
-            this.setState({
-              availableVersions: data,
-            });
+          response.json().then((responseConfig) => {
+            try {
+              const config = responseConfig.storybook.versions;
+              const versionsLocation = `${url.protocol}//${url.hostname}:${url.port}/${config.versionsPath}`;
+              const currentVersion = '0.0.1';
+
+              this.setState({
+                currentVersion,
+              });
+
+              fetch(versionsLocation).then((versionsResponse) => {
+                if (versionsResponse.ok) {
+                  versionsResponse.json().then((data) => {
+                    this.setState({
+                      availableVersions: data,
+                    });
+                  });
+                }
+              }).catch((e) => {
+                // Maybe we want to remove the div?
+                console.log(e);
+              });
+            } catch (e) {
+              // Maybe we want to remove the div?
+              console.log(e);
+            }
           });
         }
-      }).catch(() => {
-        // Ignore. Maybe we want to remove the div?
+      }).catch((e) => {
+        // Maybe we want to remove the div?
+        console.log(e);
       });
     }
   }
 
   render() {
-    const {availableVersions, currentVersion} = this.state;
+    const { availableVersions, currentVersion } = this.state;
     return (
       <div id="versions-storyWrapper">
         <Versions availableVersions={availableVersions} currentVersion={currentVersion} />
