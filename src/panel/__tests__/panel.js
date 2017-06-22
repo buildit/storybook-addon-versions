2772e56113a1924de5ce11a367e342c78436b298
Panos Voudouris <panosvoudouris@gmail.com>
2017-06-22 19:02:08 +0100

    Remove unused package

--- src/panel/__tests__/panel.js
+++ src/panel/__tests__/panel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import Panel from '../';
 
 jest.mock('../../utils/config');
